e972cd082458ce4b9eace267b4024c244e4a138c
RITZ_ <ritz1014.ritz1014@gmail.com>
2026-07-20 11:03:51 +0900

    chore(api): setup crate and add health endpoint

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, http::StatusCode, routing::get};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "api=debug".parse().unwrap()),
+        )
+        .init();
+
+    let app = Router::new().route("/health", get(health));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health() -> StatusCode {
+    StatusCode::OK
 }
